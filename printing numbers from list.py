f95c1cc78a5fcee41ac5d147be01d926ed54a5f6
laiba sagheer 1234 <54498085+laibasagheer@users.noreply.github.com>
2020-04-19 07:33:43 -0700

    Add files via upload

---
+++ printing numbers from list.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+i=1
+while i<10:
+    print(i)
+    i=i+1
+        
+
+
+# In[2]:
+
+
+list1 =[1,2,67,62,-45,-34,45,-4]
+for x in list1:
+    if x>0:
+        print(x)
+    
+
+
+# In[3]:
+
+
+list2 = [67,93,-7856,-67,-34,56,-45]
+for x in list2:
+    if x<0:
+        print(x)
+
+
+# In[7]:
+
+
+i=1
+while i<10:
+    if i==5:
+        break
+    
+    print(i)
+    i=i+1
+        
+
+
+# In[ ]:
+
+
+
+        
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
