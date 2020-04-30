d203e690a905af17b2a206f1197d2a8badce62aa
laiba sagheer 1234 <54498085+laibasagheer@users.noreply.github.com>
2020-04-30 03:10:09 -0700

    Add files via upload

---
+++ list , tupple.py
@@ -0,0 +1,325 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+names = ["laiba" ,"areeba"]
+type(names)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[6]:
+
+
+random = ["apple ", "mango", "banana",1, 2, 4]
+print(random)
+
+
+# In[7]:
+
+
+print(random + names)
+
+
+# In[16]:
+
+
+random[2:]
+
+
+# In[17]:
+
+
+random[1:4]
+
+
+# In[27]:
+
+
+del random[1]
+print(random)
+
+
+# In[29]:
+
+
+random.append(9)
+print(random)
+
+
+# In[30]:
+
+
+random.index(1)
+
+
+# In[31]:
+
+
+fruits =("apple ", "mango", "banana","grapes")
+type(fruits)
+
+
+# In[33]:
+
+
+print(fruits)
+
+
+# In[35]:
+
+
+fruits[1:]
+
+
+# In[38]:
+
+
+print(len (fruits))
+
+
+# In[42]:
+
+
+vegetables= ("tomato", "onions", "cabbage")
+type(vegetables)
+
+
+# In[43]:
+
+
+print(vegetables + fruits)
+
+
+# numbers =
+
+# In[49]:
+
+
+numbers = [1,67,78,90,63,57321, 2, 3, 4, 5, 6, 7,8]
+numbers.sort()
+numbers
+
+
+# In[51]:
+
+
+places = {"delhi"," mumbai", "dubai" }
+type(places)
+
+
+# In[52]:
+
+
+print(len(places))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
